e00c824c1809ec584dbbb68d114459724df3b1be
Joe Ganobsik <jganobsik@gmail.com>
2019-04-19 16:29:38 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,12 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  
+  def self.first_five
+    Boat.all[0,5]
+  end
+  def self.dinghy
+    boats = Boat.all
+    boats.filter{|b| b.length < 20}
+  end
 end
